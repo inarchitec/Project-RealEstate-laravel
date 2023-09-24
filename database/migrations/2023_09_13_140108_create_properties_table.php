627db21702415da0fc580b9d551d404e4b71efe2
inarchitec <110137985+inarchitec@users.noreply.github.com>
2023-09-24 14:47:53 +0300

    update 9 24 23

--- database/migrations/2023_09_13_140108_create_properties_table.php
+++ database/migrations/2023_09_13_140108_create_properties_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string("Title");
             $table->string("Area");
             $table->string("Status");
+            $table->int("Assigned_agent");
             $table->string("Price");
             $table->string("Bedroom");
             $table->string("Parking");
@@ -29,6 +30,7 @@ return new class extends Migration
             $table->string("Images")->default('https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80');
             $table->string("Videos")->nullable();
             $table->string("Location_city");
+
             $table->string("Location_woreda_kebele");
          
          
